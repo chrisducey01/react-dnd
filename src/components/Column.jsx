83edf38df9f94b771e98a7a913237fb1b14c7bd0
Christopher Ducey <chrisducey01@me.com>
2020-01-30 21:39:09 -0700

    Initial check in.  Drag and drop working on task list.

---
+++ src/components/Column.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import styled from 'styled-components';
+import Task from './Task';
+import { Droppable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+    margin: 8px;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+`;
+const Title = styled.h3`
+    padding: 8px;
+`;
+const TaskList = styled.div`
+    padding: 8px;
+    transition: background-color 0.5s ease;
+    background-color: ${props=>(props.isDraggingOver ? 'skyblue' : 'white')};
+`;
+
+export class Column extends Component {
+    render() {
+        return (
+            <Container>
+                <Title>{this.props.column.title}</Title>
+                <Droppable droppableId={this.props.column.id}>
+                    {(provided, snapshot) => (
+                        <TaskList
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                            isDraggingOver={snapshot.isDraggingOver}
+                        >
+                            {this.props.tasks.map((task, index) => {
+                                return <Task key={task.id} task={task} index={index}/>
+                            })}
+                            {provided.placeholder}
+                        </TaskList>
+                    )}
+                </Droppable>
+            </Container>
+        )
+    }
+}
+
+export default Column
